750669157313ef55f9fccd0e3b79c554bd9fbea1
Shubham Bansal <shubhambansal15@gmail.com>
2020-06-06 12:05:58 -0500

    update prod config

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,9 +31,9 @@ set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
 
 ## Linked Files & Directories (Default None):
-set :linked_files, %w{config/database.yml}
+append :linked_files, %w{config/database.yml}
 set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system .bundle public/uploads}
-append :linked_files, 'config/credentials/production.key'
+# append :linked_files, %w{'config/credentials/production.key'}
 set :keep_releases, 5
 
 ## Defaults:
