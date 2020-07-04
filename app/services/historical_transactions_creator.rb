905a6e1b27404e5ca6f2f7c22a5205af500111ad
Shubham Bansal <shubhambansal15@gmail.com>
2020-07-04 14:35:01 -0500

    Implement the history data fetch and add tests

--- app/services/historical_transactions_creator.rb
+++ app/services/historical_transactions_creator.rb
@@ -1,5 +1,5 @@
 class HistoricalTransactionsCreator < ApplicationService
-  def initialize(access_token, user, transaction_count)
+  def initialize(access_token, user, transaction_count=0)
     @client = PlaidClientCreator.call
     @access_token = access_token
     @user = user
@@ -16,40 +16,49 @@ class HistoricalTransactionsCreator < ApplicationService
     transactions_fetched_count = 0
     transactions_json_array = []
     end_date = Date.today
-    until @transaction_count > transactions_fetched_count do
-      transactions_json_array << fetch_transactions(end_date)
+    back_off_date = Date.today - 2.years
+    until transactions_fetched_count >= @transaction_count  do
+      Rails.logger.tagged("TransactionPull") {
+        Rails.logger.info { "Total #{transactions_fetched_count}/#{@transaction_count} transactions pulled from Plaid."}
+      }
+      transactions_json_array += fetch_transactions(end_date)
       transactions_fetched_count = transactions_json_array.length
-      if end_date < (Date.today - 2.years)
+      if end_date.before?(back_off_date)
         break
       else
-        end_date = end_date - 2.month
+        end_date = end_date - 3.month
       end
     end
+    Rails.logger.tagged("TransactionPull") {
+      Rails.logger.info { "Saving #{transactions_fetched_count} transactions from Plaid into the DB."}
+    }
     Transaction.create_transactions_from_json(transactions_json_array.flatten, @user.id)
   end
 
   def fetch_transactions(end_date)
     start_date = (end_date - 2.month).iso8601
-    Rails.logger.tagged("TransactionPull") { 
+    Rails.logger.tagged("TransactionPull") {
       Rails.logger.info("Fetching transactions for start_date: #{start_date}, end_date: #{end_date}")
     }
-    transactions_response = @client.transactions.get(
-      @access_token,
-      start_date,
-      end_date
-    )
+    transactions_response = @client.transactions.get(@access_token, start_date, end_date, count: 250, offset: 0)
     transactions_json_array = transactions_response[:transactions]
-    Rails.logger.tagged("TransactionPull") { 
-      Rails.logger.info "Total transactions that need to be fetched - #{transactions_response[:total_transactions]}"
+    total_tx_count = transactions_response[:total_transactions]
+    Rails.logger.tagged("TransactionPull") {
+      Rails.logger.info "Total transactions that need to be fetched - #{total_tx_count}"
     }
-    while transactions_json_array.length < transactions_response[:total_transactions]
+    while transactions_json_array.length < total_tx_count
       transactions_response = @client.transactions.get(
         @access_token,
-        @start_date_iso8601_str,
-        @end_date_iso8601_str
+        start_date,
+        end_date,
+        count: 250,
+        offset: transactions_json_array.length,
       )
-      transactions_json_array << transactions_response[:transactions]
+      transactions_json_array += transactions_response[:transactions]
     end
+    Rails.logger.tagged("TransactionPull") {
+      Rails.logger.info "#{transactions_json_array.count}/#{total_tx_count} transactions fetched from Plaid."
+    }
     transactions_json_array
   end
 end
