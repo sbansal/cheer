7fb8e04be0475b1ea3e928606e05e403c0e79f99
Shubham Bansal <shubhambansal15@gmail.com>
2020-08-18 00:10:00 -0500

    fix the active menu items

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def active_menu_item(nav_item)
+    case nav_item
+    when "home"
+      "active " if (controller_name == "dashboard" && action_name == "index")
+    when "transactions"
+      "active " if (controller_name == "transactions" && action_name == "index")
+    when "bank_accounts"
+      "active " if (controller_name == "bank_accounts" && action_name == "index")
+    when "login_items"
+      "active " if (controller_name == "login_items" && action_name == "index")
+    when "account_settings"
+      "active " if (controller_name == "accounts" && action_name == "settings")
+    else
+      ""
+    end
+  end
 end
