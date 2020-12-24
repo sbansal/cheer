9c27108949b2190f3e5996477255b8908847b0ef
Shubham Bansal <shubhambansal15@gmail.com>
2020-12-24 14:04:38 -0600

    add new line to files

--- app/javascript/controllers/tfa_controller.js
+++ app/javascript/controllers/tfa_controller.js
@@ -4,7 +4,6 @@ import Rails from "@rails/ujs"
 export default class extends Controller {
 
   toggle(event) {
-    console.log("toggling 2FA")
     const enable_tfa = document.getElementById('tfa-switcher').checked
     if (enable_tfa) {
       Rails.ajax({
