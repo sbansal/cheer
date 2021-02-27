20ed58975fa959db4bb10d2cde2bafe287b938ea
Shubham Bansal <shubhambansal15@gmail.com>
2021-02-27 17:06:41 -0600

    Fixed filtering of params for sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,6 +2,11 @@ Sentry.init do |config|
   config.dsn = Rails.application.credentials.sentry[:dsn]
   config.breadcrumbs_logger = [:active_support_logger]
   config.enabled_environments = %w[production]
+  filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
+  config.before_send = lambda do |event, hint|
+    event.request.data = filter.filter(event.request.data)
+    event
+  end
 
   # To activate performance monitoring, set one of these options.
   # We recommend adjusting the value in production:
@@ -14,6 +19,4 @@ Sentry.init do |config|
   config.async = lambda do |event, hint|
     Sentry::SendEventJob.perform_later(event, hint)
   end
-
-  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
 end
