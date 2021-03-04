f8fda063d4b5fd3b891ab0aa4405df430d60c76e
Shubham Bansal <shubhambansal15@gmail.com>
2021-03-04 16:23:58 -0600

    remove the filtering logic for sentry request since it is already supported out of the box

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,11 +2,6 @@ Sentry.init do |config|
   config.dsn = Rails.application.credentials.sentry[:dsn]
   config.breadcrumbs_logger = [:active_support_logger]
   config.enabled_environments = %w[production]
-  filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
-  config.before_send = lambda do |event, hint|
-    event.request.data = filter.filter(event.request.data)
-    event
-  end
 
   # To activate performance monitoring, set one of these options.
   # We recommend adjusting the value in production:
