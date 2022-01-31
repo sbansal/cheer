8d610ac4c8561acecb9077f37ee22fefe9270620
Shubham Bansal <shubhambansal15@gmail.com>
2022-01-30 19:30:42 -0600

    Adding more logging information for balance processor

--- app/services/plaid_balance_processor.rb
+++ app/services/plaid_balance_processor.rb
@@ -2,12 +2,13 @@ class PlaidBalanceProcessor < ApplicationService
   def initialize(access_token)
     @client = PlaidClientCreator.call
     @access_token = access_token
+    @login_item = LoginItem.find_by(plaid_access_token: @access_token)
   end
 
   def call
     begin
-      if LoginItem.find_by(plaid_access_token: @access_token).expired?
-        Rails.logger.warn("[PlaidBalanceProcessor] LoginItem is expired hence not refreshing the balance.")
+      if @login_item.expired?
+        Rails.logger.warn("[PlaidBalanceProcessor] LoginItem with ID=#{@login_item.item} is expired hence not refreshing the balance.")
       else
         request = Plaid::AccountsBalanceGetRequest.new({ access_token: @access_token })
         response = @client.accounts_balance_get(request)
@@ -15,7 +16,7 @@ class PlaidBalanceProcessor < ApplicationService
         BankAccount.update_balances(accounts_json)
       end
     rescue => e
-      Rails.logger.error("[PlaidBalanceProcessor] Unable to retrieve balance for login item.")
+      Rails.logger.error("[PlaidBalanceProcessor] Unable to retrieve balance for login item with ID=#{@login_item.id}")
       Rails.logger.error(e)
     end
   end
