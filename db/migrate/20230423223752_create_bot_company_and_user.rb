c9ec84287149094f47ef7a3fc6d5e7d9c1467d39
Shubham Bansal <shubhambansal15@gmail.com>
2023-06-19 18:53:01 -0500

    Update migration

--- db/migrate/20230423223752_create_bot_company_and_user.rb
+++ db/migrate/20230423223752_create_bot_company_and_user.rb
@@ -1,6 +1,6 @@
 class CreateBotCompanyAndUser < ActiveRecord::Migration[7.0]
   def change
-    company = Company.create(name: 'PrivatFi Bot Army', free_account: true)
+    company = Company.create(name: 'PrivatFi Bot Army')
     user = User.new(email: 'privatefi@usecheer.com', company: company, full_name: 'PrivateFi', account_owner: true, password: SecureRandom.hex(8))
     user.skip_confirmation!
     user.save!
